d381bcbe47751fd1a865f190d275567c340c2f61
Park, JungHwan <park930919@gmail.com>
2016-04-22 18:52:45 +0900

    Update

--- WindowBase.cpp
+++ WindowBase.cpp
@@ -2,7 +2,7 @@
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 HINSTANCE g_hInst;
-LPCTSTR lpszClass= TEXT("First");
+LPCTSTR lpszClass= TEXT("First"); //클래스
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow)
 {
@@ -11,17 +11,17 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmd
 	WNDCLASS WndClass;
 	g_hInst = hInstance;
 	
-	WndClass.cbClsExtra = 0;
-	WndClass.cbWndExtra = 0;
-	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
-	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	WndClass.hInstance = hInstance;
-	WndClass.lpfnWndProc = WndProc;
-	WndClass.lpszClassName = lpszClass;
-	WndClass.lpszMenuName = NULL;
-	WndClass.style = CS_HREDRAW | CS_VREDRAW;
-	RegisterClass(&WndClass);
+	WndClass.cbClsExtra = 0;										//예약영역1, 특수한 경우 사용되며 그외는 0으로
+	WndClass.cbWndExtra = 0;										//예약영역2, 이하 동일
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);   //윈도우의 배경 색상을 지정.
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);					//마우스 커서 지정.
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);				//아이콘 지정.
+	WndClass.hInstance = hInstance;									//윈도우 클래스를 등록하는 프로그램의 번호로 WinMain의 hInstance를 주면 된다.
+	WndClass.lpfnWndProc = WndProc;									//메세지를 처리할 함수.(중요!)
+	WndClass.lpszClassName = lpszClass;								//프로그램 클래스의 이름 등록.
+	WndClass.lpszMenuName = NULL;									//프로그램이 사용할 메뉴를 지정한다.
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;						//윈도우 스타일(윈도우가 어떤 형태를 갖을 지의 값들)
+	RegisterClass(&WndClass);										//WndClass 특성을 저장.
 
 	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, (HMENU)NULL, hInstance, NULL);
 	ShowWindow(hWnd,nCmdShow);
